cb067b2daf83fbec915eafe334079a378afec14a
Matias <mati.delarosa1984@gmail.com>
2024-03-24 14:02:46 -0300

    commit app

---
+++ client/src/utils/links.js
@@ -0,0 +1,13 @@
+import { TbChartBar } from "react-icons/tb";
+import { MdQueryStats } from 'react-icons/md';
+import { SiReacthookform } from "react-icons/si";
+import { ImProfile } from 'react-icons/im';
+
+const links = [
+  { id: 1, text: 'stats', path: '/',  },
+  { id: 2, text: 'all jobs', path: 'all-jobs', },
+  { id: 3, text: 'add job', path: 'add-job', },
+  { id: 4, text: 'profile', path: 'profile',  },
+];
+
+export default links;
